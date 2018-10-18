0993e0c011858f9c20f4b0e4fa62286385eb3a1d
Arunangshu <arunangshubsws@gmail.com>
2018-10-18 21:58:03 +0530

    Implemented the skeleton of components required in Home component
    Signed-off-by: Arunangshu <arunangshubsws@gmail.com>

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import Header from '../elements/Header/Header';
-import Home from '../Home/Home';
+import React from "react";
+import Header from "../elements/Header/Header";
+import Home from "../Home/Home";
 
 const App = () => {
-    return ( 
-        <div>
-            <Header />
-            <Home />
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <Header />
+      <Home />
+    </div>
+  );
+};
+
 export default App;

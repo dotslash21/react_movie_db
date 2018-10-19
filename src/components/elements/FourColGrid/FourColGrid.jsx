b1fd46586b5d548c6ef768248ed94aba73e1ca27
Arunangshu <arunangshubsws@gmail.com>
2018-10-20 03:15:37 +0530

    Implemnted proptype checking for ease of debugging
    Signed-off-by: Arunangshu <arunangshubsws@gmail.com>

--- src/components/elements/FourColGrid/FourColGrid.jsx
+++ src/components/elements/FourColGrid/FourColGrid.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./FourColGrid.css";
 
 const FourColGrid = props => {
@@ -20,6 +21,11 @@ const FourColGrid = props => {
       <div className="rmdb-grid-content">{renderElements()}</div>
     </div>
   );
+};
+
+FourColGrid.propTypes = {
+  header: PropTypes.string,
+  loading: PropTypes.bool.isRequired
 };
 
 export default FourColGrid;
